6b47c9760a8d5ad67ac0a60773ef8cb54eed69da
LiuYong <793875613@qq.com>
2022-04-09 20:37:59 +0800

    add sample code for autowired bean creation

---
+++ boot4go.go
@@ -0,0 +1,20 @@
+package boot4go
+
+/**
+* golang-sample源代码，版权归锦翰科技（深圳）有限公司所有。
+* <p>
+* 文件名称 : boot4go.go
+* 文件路径 :
+* 作者 : DavidLiu
+× Email: david.liu@ginghan.com
+*
+* 创建日期 : 2022/4/9 20:37
+* 修改历史 : 1. [2022/4/9 20:37] 创建文件 by NST
+*/
+
+const (
+	L4G_VERSION = "boot4go-v1.0.0"
+	L4G_MAJOR   = 1
+	L4G_MINOR   = 0
+	L4G_BUILD   = 0
+)
